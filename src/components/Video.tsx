443e8cd508184bbd6ae3d3c54bdeec28e0180762
HOANG~ <hoangne747@gmail.com>
2025-10-30 09:32:24 +0700

    Remove package-lock.json to use yarn.lock exclusively and update-browserslist

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -34,14 +34,14 @@ export function Video({ videoId }: Readonly<VideoProps>) {
             <span className="sr-only">Play Video</span>
           </button>
         )}
-        {playVideo && (
-          <iframe
-            src={`https://www.youtube-nocookie.com/embed/${videoId}?controls=0&autoplay=1`}
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            className="w-full h-full aspect-video"
-          ></iframe>
-        )}
+        {playVideo && (
+          <iframe
+            src={`https://www.youtube-nocookie.com/embed/EUQTu3pDcvE?controls=0&autoplay=1`}
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            className="w-full h-full aspect-video"
+          ></iframe>
+        )}
       </div>
     </Container>
   );
